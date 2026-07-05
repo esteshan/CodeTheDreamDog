d2f0abcdf9a68440f0d0f266f7a41f2d256d27ec
esteshan <esteshan100@gmail.com>
2026-07-05 16:53:37 -0700

    checkpoint now need to add my api key to get the dog breeds

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const fetchPromise = fetch(
     "https://api.thedogapi.com/v1/breeds"
-)
+);
 
 // State of the fetchPromise
-console.log(fetchPromise)
+console.log(fetchPromise);
 // Let user know that data is being fetched
-console.log("Fetching data...")
+console.log("Fetching data...");
 
 // Get response status
 /*fetchPromise.then((response) => {
@@ -14,10 +14,25 @@ console.log("Fetching data...")
 
 
 // Get response data as JSON
-fetchPromise.then((response) => {
+/*fetchPromise.then((response) => {
     const jsonPromise = response.json()
 
     jsonPromise.then((data) => {
         console.log(data)
     })
-});
+});*/
+
+// Correct way of calling using .then
+fetchPromise
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].name);
+    })
+    .catch((error) => {
+      console.error(`Could not get pet breeds: ${error}`);
+    });
