1d6076bae0fde4969e737f169b4937e1d16407ef
esteshan <esteshan100@gmail.com>
2026-07-05 17:25:36 -0700

    fixed vite by adding the npm run dev script, also added added ts file so intelij would not throw a warning that it dosent understand the api key from the env

--- api.js
+++ api.js
@@ -1,5 +1,9 @@
+const apiKey = import.meta.env.VITE_DOG_API;
+
 const fetchPromise = fetch(
-    "https://api.thedogapi.com/v1/breeds"
+    "https://api.thedogapi.com/v1/breeds", {
+      headers: { "x-api-key": apiKey },
+    }
 );
 
 // State of the fetchPromise
@@ -7,22 +11,6 @@ console.log(fetchPromise);
 // Let user know that data is being fetched
 console.log("Fetching data...");
 
-// Get response status
-/*fetchPromise.then((response) => {
-    console.log(`Received response ${response.status}`)
-});*/
-
-
-// Get response data as JSON
-/*fetchPromise.then((response) => {
-    const jsonPromise = response.json()
-
-    jsonPromise.then((data) => {
-        console.log(data)
-    })
-});*/
-
-// Correct way of calling using .then
 fetchPromise
     .then((response) => {
       if (!response.ok) {
